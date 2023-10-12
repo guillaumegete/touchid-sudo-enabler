a442ad41dcb2174ae2d9f30e1329e7fc8c4bfd53
Guillaume Gète <59868217+guillaumegete@users.noreply.github.com>
2023-10-12 13:10:40 +0200

    Create touchid-sudo-enabler.sh

---
+++ touchid-sudo-enabler.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+OSVERSION=$(sw_vers -buildVersion | cut -b 1-2)
+
+# macOS builds :
+# Mojave = 18
+# Catalina = 19
+# Big Sur = 20
+# Monterey = 21
+# Ventura = 22
+# Sonoma = 23
+
+echo "macOS Build : version $OSVERSION"
+
+# Running macOS Sonoma or higher
+
+if [ "$OSVERSION" -gt "22" ]; then
+
+# in macOS Sonoma and higher we just need to comment the pam_tid.so line in /etc/pam.d/sudo_local.template
+
+cp /etc/pam.d/sudo_local.template /etc/pam.d/sudo_local.template_backup
+
+sed -i ''  '/pam_tid.so/ s/^#//' /etc/pam.d/sudo_local.template
+
+else
+	
+	# Running macOS Ventura or lower
+
+	cp /etc/pam.d/sudo /etc/pam.d/sudo.backup
+
+# in macOS Ventura and lower the pam_tid.so line does not exist so we must add it after the first comment line.
+ 
+sed -i '' "/\#/a\ 
+auth       sufficient     pam_tid.so\\
+" /etc/pam.d/sudo
+	
+fi
+
+exit 0
